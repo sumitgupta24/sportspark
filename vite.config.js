eb9f8d02f65a9d440c5cb7ed964e913d725f9217
Sumit Gupta <cricsumit24@gmail.com>
2025-07-19 11:04:25 +0530

    Fix vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // ✅ Set the repo name here
 export default defineConfig({
   plugins: [react()],
-  base: '/sportspark/', // 👈 Important for GitHub Pages
+  base: '', // 👈 Important for GitHub Pages
 });
